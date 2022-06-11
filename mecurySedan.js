bb5c0bb2e81a27f8ec50cc0acae6de8b73cca579
Crystal Korth <crystalkorth@gmail.com>
2022-06-10 19:10:23 -0500

    added extends and imported vehicle from vehicle.js

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,18 @@
+module.import = {
+    Vehicle
+} from './vehicle.js';
+
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends class Vehicle{
+    constructor(make, model, year, color, mileage) {
+
+   
+    super(maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel, scheduleService)
+}
+}
