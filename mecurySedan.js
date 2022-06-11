459c7aaefd4fb784659c090efa0f8975e92691f4
Crystal Korth <crystalkorth@gmail.com>
2022-06-10 19:30:11 -0500

    loading passenger....

--- mecurySedan.js
+++ mecurySedan.js
@@ -9,17 +9,22 @@ const VehicleModule = require("./vehicleBaseClass")
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-class Car extends class Vehicle{
+class Car extends class VehicleModule{
     constructor(make, model, year, color, mileage) {
 
    
     super(make, model, year, color, mileage);
-    this.maximumPassengers
-    this.passenger
-    this.numberOfWheels
-    this.maximumSpeed
-    this.fuel
-    this.scheduleService
+    this.maximumPassengers = '5';
+    this.passenger = '0';
+    this.numberOfWheels = '4';
+    this.maximumSpeed = '160';
+    this.fuel = '10';
+    this.scheduleService = 'false';
     
+}
+loadPassenger (num) {
+    (if this.passenger < this.maximumPassengers) {  //leaving off here for the moment....
+
+    }
 }
 }
